6f5e98156c97807e64351463960a5210b58cd67c
Matteo Guarnaccia <matteo.guarnaccia@stfc.ac.uk>
2025-07-28 11:37:44 +0100

    added last_scanned field to user's database

--- xfc_control/admin.py
+++ xfc_control/admin.py
@@ -19,11 +19,11 @@ admin.site.register(CacheDisk, CacheDiskAdmin)
 class UserAdmin(admin.ModelAdmin):
     save_on_top = True
     list_display = ('name', 'email', 'notify', 'formatted_size', 'formatted_used',
-                    'formatted_hard_limit', 'formatted_total_used','cache_disk', 'cache_path')
+                    'formatted_hard_limit', 'formatted_total_used','cache_disk', 'cache_path', 'last_scanned')
     fields = ('name', 'email', 'notify', 'quota_size', 'formatted_used',
-              'hard_limit_size', 'formatted_total_used', 'cache_disk', 'cache_path')
+              'hard_limit_size', 'formatted_total_used', 'cache_disk', 'cache_path', 'last_scanned')
     search_fields = ('name', 'email')
-    readonly_fields = ('email', 'formatted_used', 'formatted_total_used')
+    readonly_fields = ('email', 'formatted_used', 'formatted_total_used', 'last_scanned')
 admin.site.register(User, UserAdmin)
 
 class UserLockAdmin(admin.ModelAdmin):
